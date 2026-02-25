35f9040286e1c84455102cb8b6d271751df72336
Enzo <enzo@gmail.com>
2026-02-25 12:46:23 +0100

    my seconf commit

--- arrayslice/array.go
+++ arrayslice/array.go
@@ -6,12 +6,24 @@ func main() {
 	// ARRAYS
 
 	var name = [3]string{"Enzo", "John", "Kito"}
-	var cars = [4]string{"Benz", "Toyota", "Lexus", "Audi"}
+	var cars = []string{"Benz", "Toyota", "Lexus", "Audi"}
 	cars[3] = "RTOS"
 
-	fmt.Println(name)
+	cars = append(cars, "Lamborghini")
+
+	fmt.Println(name[0])
 	fmt.Println(cars, len(cars))
 
 	// SLICES
+	var scores = []int{23, 25, 18, 21, 16, 21, 19}
+	scores[5] = 20
 
+	scores = append(scores, 30)
+
+	fmt.Println(scores)
+
+	// SLICES RANGE
+	rangeOne := cars[2:]
+
+	fmt.Println(rangeOne)
 }
