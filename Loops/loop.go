b1b01f47ce50b29fa5969701ab951f2e3e1cc224
Enzo <enzo@gmail.com>
2026-02-26 11:25:16 +0100

    third commit

---
+++ Loops/loop.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	// longer version
+
+	// var x = 0
+
+	// // for x < 5 {
+	// // 	fmt.Println("the value of x is: ", x)
+
+	// // 	x++
+	// // }
+
+	// Shorter Version
+
+	for x := 0; x < 5; x++ {
+		fmt.Println("the value of x is: ", x)
+	}
+}
